c2d1857a94f6b8c02f59512eda044b357c30bf26
Lex_TM <BenuelBertran@gmail.com>
2018-11-07 10:41:00 +0300

    Начал вести историю проекта Code & Magic

---
+++ js/game--modify.js
@@ -0,0 +1,18 @@
+/*jslint node: true */
+"use strict";
+
+var fireballSize = 22;
+var getFireballSpeed = function (left) {
+  return left ? 5 : 2;
+};
+var wizardSpeed = 3;
+var wizardWidth = 70;
+var getWizardHeigth = function () {
+  return 1.337 * wizardWidth;
+};
+var getWizardX = function (width) {
+  return width / 2;
+};
+var getWizardY = function (height) {
+  return height / 3 * 2;
+};
